b68f2bbc8008e354506a5f6c85a9ce8ac5168e56
Happy-wada <c1100471@planet.kanazawa-it.ac.jp>
2022-04-07 11:25:14 +0900

    first commit

---
+++ src/Keyboard_Control1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rospy
+from geometry_msgs.msg import Twist
+
+def talker():
+    rospy.init_node("teleop")
+    pub = rospy.Publisher('/vmegarover/diff_drive_controller/cmd_vel', Twist, queue_size=1)
+    while not rospy.is_shutdown():
+        vel = Twist()
+        direction = raw_input ("push_keyboard:,,,")
+        #前に進む
+        if "" in direction:
+            vel.linear.x = +
+        #後ろに進む
+        if "" in direction:
+            vel.linear.x = -
+        #右に曲がる
+        if "" in direction:
+            vel.angular.z = +
+        #左に曲がる
+        if "" in direction:
+            vel.angular.z = -
+
+        pub.publish(vel)
+
+if __name__ == '__main__':
+    talker()
